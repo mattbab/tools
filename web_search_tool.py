cf3626d377862f01494e2626bffec91e823b85c4
Jim <JimBabcock59@users.noreply.github.com>
2024-02-20 15:10:31 -0600

    Update web_search_tool.py
    before fixing wikipedia

--- web_search_tool.py
+++ web_search_tool.py
@@ -3,6 +3,17 @@ from langchain.chat_models import ChatOllama
 
 def setup(ToolManager):
     ToolManager.add(web_search_tool)
+    ToolManager.add(science_archive_search_tool)
+    ToolManager.add(medical_search_tool)
+    ToolManager.add(wikipedia_search_tool)
+    ToolManager.add(web_archive_search_tool)
+    ToolManager.add(code_search_tool)
+    ToolManager.add(math_search_tool)
+    ToolManager.add(video_search_tool)
+    ToolManager.add(image_search_tool)
+    ToolManager.add(product_search_tool)
+    ToolManager.add(music_search_tool)
+    ToolManager.add(street_map_search_tool)
 
 """
     Call a SearxNG server with a specified query and additional optional parameters.
@@ -127,6 +138,448 @@ print(results)
 
 llm = ChatOllama(model="llama2", temperature=0)
 
+
+def science_archive_search_tool(query:str)->str:
+    """
+    This tool is best for searching research papers.
+    """
+    print("Searching research papers...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="arxiv",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at understanding research articles in Physics, Mathematics, Computer Science, Quantitative Biology, Quantitative Finance, and Statistics. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
+def medical_search_tool(query:str)->str:
+    """
+    This tool is best for searching medical research papers.
+    """
+    print("Searching medical research papers...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="pubmed",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at understanding research articles in Medicine. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
+def wikipedia_search_tool(query:str)->str:
+    """
+    This tool is useful for when you need to answer general questions about 
+        people, places, companies, facts, historical events, and concepts.
+    """
+    print("Searching wikipedia...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="wikipedia",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at answering general questions about 
+        people, places, companies, facts, historical events, and concepts. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
+def web_archive_search_tool(query:str)->str:
+    """
+    This tool is best for searching historical web content.
+    """
+    print("Searching web archive...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="archiveorg",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at  researching historical web content. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
+def code_search_tool(query:str)->str:
+    """
+    This tool is best for finding answers to software and hardware questions.
+    """
+    print("Searching github...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="github,gitlab,stackexchange",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at answering questions about software and hardware. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
+def video_search_tool(query:str)->str:
+    """
+    This tool is best for searching video content.
+    """
+    print("Searching video content...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="youtube,vimeo,dailymotion",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at researching video content. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
+def image_search_tool(query:str)->str:
+    """
+    This tool is best for answering questions about images.
+    """
+    print("Searching image content...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="flickr,unsplash",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at understanding images and photos. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
+def product_search_tool(query:str)->str:
+    """
+    This tool is best for doing product searches on Amazon and Ebay.
+    """
+    print("Searching Amazon and Ebay...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="amazon,ebay",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at performing product searches on Amazon and Ebay. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
+def music_search_tool(query:str)->str:
+    """
+    This tool is best for searching music lyrics and music related content.
+    """
+    print("Searching music lyrics...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="genius",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at understanding music lyrics and music related content. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
+def street_map_search_tool(query:str)->str:
+    """
+    This tool is best for understanding geographical and map-related information.
+    """
+    print("Searching research papers...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="openstreetmap",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at understanding geographical and map-related information. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
+def math_search_tool(query:str)->str:
+    """
+This tool is best for answering queries about mathematics, science, engineering, and society.    """
+    print("Searching Wolfram Alpha...")
+    se_resp = web_search(
+        query, 
+        format="json", 
+        engines="wolframalpha",
+    )
+    se_url = '\n'.join(result['url'] for result in se_resp['results'][:3])
+    se_resp = '\n'.join(result['content'] for result in se_resp['results'][:3])
+    reassess_instructionsextract_instructions = f"""
+    System: 
+    <System>
+    You are an expert at answering queries about mathematics, science, engineering, and society. 
+    </System>
+    
+    Instructions: 
+    <Instructions>
+    You are to extract from the Background Information find the best, most current and concise response to the Search Request and rewrite the response to be concise.
+    </Instructions>
+
+    Background Information: 
+    <Background Information>
+    {se_resp}
+    </Background Information>
+
+    Search Request: 
+    <Search Request>
+    {query}
+    </Search Request>
+    """
+    search_result = llm.invoke(reassess_instructionsextract_instructions) 
+    output = f"""{search_result.content}
+
+    Citations: {se_url}
+    """
+    return output
+
 def web_search_tool(query:str)->str:
     """
     This tool is best for searching the Internet.
@@ -213,26 +666,29 @@ def print_results(results):
 
 def main():
     # Example usage
-    query = "Who won the Iowa caucus in 2024?"
-    results = web_search(
-        query, 
-        format="json", 
-        lang="en", 
-        time_range="week", 
-        safesearch=1, 
-        engines="bing", 
-        categories="general",
-        image_proxy=True,
-        autocomplete="off",
-        origin="us",
-        enable_http=True,
-        no_redirect=True,
-        no_cache=True,
-        pageno=1,
-        pagesize=10
-    )
+    # query = "Who won the Iowa caucus in 2024?"
+    # results = web_search(
+    #     query, 
+    #     format="json", 
+    #     lang="en", 
+    #     time_range="week", 
+    #     safesearch=1, 
+    #     engines="bing", 
+    #     categories="general",
+    #     image_proxy=True,
+    #     autocomplete="off",
+    #     origin="us",
+    #     enable_http=True,
+    #     no_redirect=True,
+    #     no_cache=True,
+    #     pageno=1,
+    #     pagesize=10
+    # )
 
-    print(results)
+    # wikipedia
+
+    # print(results)
+    print(wikipedia_search_tool("""January 6th"""))
 
 if __name__ == "__main__":
     main()
